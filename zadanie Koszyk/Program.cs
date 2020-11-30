c5d2e1f378c4f8ff8a004d8db8babf926093ef19
Wiktoria Sitko <wsitko2000@gmail.com>
2020-11-30 12:34:42 +0100

    New program

---
+++ zadanie Koszyk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zadanie_Koszyk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
