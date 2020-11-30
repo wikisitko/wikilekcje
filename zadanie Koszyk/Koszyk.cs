8ce7c47e122d9f980c7509ba861bc457f551445b
Wiktoria Sitko <wsitko2000@gmail.com>
2020-11-30 23:53:33 +0100

    Added 2 classes

--- zadanie Koszyk/Koszyk.cs
+++ zadanie Koszyk/Koszyk.cs
@@ -1,29 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace zadanie_Koszyk
 {
-    class Program
-    {
-        static void Main(string[] args)
+    class Koszyk
+    { 
+        public List<Telefon> ListaTelefonow;
+
+
+        public Koszyk() => ListaTelefonow = new List<Telefon>();
+
+
+        void DodajProdukt(string marka1, string model1, int cena1)
         {
-            
-            Telefon t1 = new Telefon("Samsung","A30", 100);
+            /*Telefon t1 = new Telefon("Samsung", "A30", 100);
             Telefon t2 = new Telefon("Samsung", "A40", 1000);
             Telefon t3 = new Telefon("Samsung", "A50", 10000);
             Telefon t4 = new Telefon("Samsung", "A60", 100000);
 
-
-            List<Telefon> ListaTelefonow = new List<Telefon>();
             ListaTelefonow.Add(t1);
             ListaTelefonow.Add(t2);
             ListaTelefonow.Add(t3);
-            ListaTelefonow.Add(t4);
-
-
-           
-
-            Console.WriteLine("Hello World!");
+            ListaTelefonow.Add(t4);*/
+        }
+        
+        void Cena(int cena)
+        {
+            
         }
     }
 }
---
+++ zadanie Koszyk/Telefon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zadanie_Koszyk
+{
+    class Telefon
+    {
+        private string marka;
+        private string model;
+        private int cena;
+
+        public Telefon(string marka1, string model1, int cena1)
+        {
+            this.marka = marka1;
+            this.model = model1; 
+            this.cena = cena1;
+            
+        }
+       
+
+        public override string ToString()
+        {
+            return $"Marka telefonu to {marka}, model to {model}, a cena to {cena}";
+        }
+    }
+
+}
