06c5d8d989395633e8b486a5c95b14c8ec25549f
Wiktoria Sitko <wsitko2000@gmail.com>
2020-12-14 23:27:54 +0100

    5 exercises

---
+++ zadanka3/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace zadanka3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////ZADANIE1
+
+            /*Console.Write("a: ");
+            int a = Int32.Parse(Console.ReadLine());
+
+            Console.Write("b: ");
+            int b = Int32.Parse(Console.ReadLine());
+
+            for (int i = b; i >= a; i--)
+            {
+                Console.WriteLine(i);
+            } */
+
+            ////ZADANIE 2
+            /*Console.Write("a: ");
+            int a = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= a; i++)
+            {
+                Console.WriteLine(Math.Pow(i, 2));
+            }*/
+
+            ////ZADANIE 3
+            /*Console.Write("a: ");
+            int a = Int32.Parse(Console.ReadLine());
+
+            Console.Write("b: ");
+            int b = Int32.Parse(Console.ReadLine());
+
+            int s = 0;
+
+            for (int i = a; i <= b; i++)
+            {
+                s += i;                
+            }
+            Console.WriteLine(s); */
+
+            ////ZADANIE 4 - może jeszcze wymyślę...
+            /*Console.Write("a: ");
+            int a = Int32.Parse(Console.ReadLine());
+
+            Console.Write("b: ");
+            int b = Int32.Parse(Console.ReadLine());
+            int s = 1;
+
+            for (int i = a; i <= b; i++)
+            {
+
+                do
+                {
+                    s = s * i;
+                }
+                while (s < 1000);
+
+                Console.WriteLine(s);
+            } */
+
+            ////ZADANIE 9
+            Console.Write("1: ");
+            int first = Int32.Parse(Console.ReadLine());
+
+            Console.Write("2: ");
+            int second = Int32.Parse(Console.ReadLine());
+
+            Console.Write("3: ");
+            int third = Int32.Parse(Console.ReadLine());
+
+            Console.Write("4: ");
+            int fourth = Int32.Parse(Console.ReadLine());
+
+            Console.Write("5: ");
+            int fifth = Int32.Parse(Console.ReadLine());
+
+            
+
+            ////ZADANIE 6
+            ////ZADANIE 6
+
+            
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
