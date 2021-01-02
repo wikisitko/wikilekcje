f84a38b8e37fa49f5c9bb912afd77bd71980d12c
Wiktoria Sitko <wsitko2000@gmail.com>
2021-01-02 15:02:38 +0100

    pierwsza faza

--- programLekcje/Program.cs
+++ programLekcje/Program.cs
@@ -5,10 +5,37 @@ namespace programLekcje
 {
     class Program
     {
+           
+        
         static void Main(string[] args)
         {
-            string dateText = File.ReadAllText(@"C:\Users\Wiktoria Sitko\Documents\GitHub\wikilekcje\programLekcje\lekcje.txt");
-            Console.WriteLine(dateText);
+            string Path = @"C:\Users\Wiktoria Sitko\Documents\GitHub\wikilekcje\programLekcje\lekcje.csv";
+            var lines = File.ReadAllLines(Path);
+            string payment;
+
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                var items = lines[i].Split(",");
+                string date = items[0];
+                payment = items[1];
+                string name = items[2];
+                int price = Int32.Parse(items[3]);
+
+                //Console.WriteLine($"{date}  {payment}  {name}  {price}");
+            }
+
+            bool czyPing ()
+            {
+                if (payment == "ping")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
 
 
         }
---
+++ zadanka5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace zadanka5
+{
+    class Program
+    {
+        static void Zadanie1()
+        {
+            int input = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < input-1; i++)
+            {
+                for (int j = 0; j < input; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+
+        static void Zadanie2()
+        {
+            string zdanie = Console.ReadLine();
+
+            for (int i = 0; i < zdanie.Length; i++)
+            {
+                if (zdanie[i] == 'i')
+                {
+                    //??
+                }
+            }
+        }
+
+        static void Zadanie3()
+        {
+            Random rand = new Random();
+
+        }
+
+        static void Main(string[] args)
+        {
+            Zadanie1();
+            Console.ReadLine();
+        }
+    }
+}
