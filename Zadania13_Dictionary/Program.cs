e9e5c85bc6add126e3ae7ea1846e7e6af0a8fb26
Wiktoria Sitko <wsitko2000@gmail.com>
2021-03-10 14:08:15 +0100

    Brakuje buildera

---
+++ Zadania13_Dictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Zadania13_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Alan", "Balan");
+            //s1.wyniki.Add(EgzaminTyp.ASiD, 5);
+            s1.DodajWynik(EgzaminTyp.ASiD, 5);
+            Console.WriteLine(s1);
+
+            Student s2 = new Student("Marek", "Barek");
+            s2.DodajWynik(EgzaminTyp.PROE, 5);
+            Console.WriteLine(s2);
+        }
+    }
+}
---
+++ Zadania13_Dictionary/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadania13_Dictionary
+{
+    public enum EgzaminTyp { PROE, PRM, ASiD }
+    class Student
+    {
+        protected string imie;
+        protected string nazwisko;
+        //private EgzaminTyp TypEgzaminu;
+
+        public Student(string imie, string nazwisko)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+        }
+
+        Dictionary<EgzaminTyp, int> wyniki = new Dictionary<EgzaminTyp, int>();
+
+        //public Dictionary<EgzaminTyp, int> Wyniki { get => wyniki; set => wyniki = value; }
+        public void DodajWynik(EgzaminTyp Exam, int ocena)
+        {
+            wyniki.Add(Exam, ocena);
+        }
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append($"");
+            foreach (var item in wyniki)
+            {
+                builder.Append($"Imie: {imie}, nazwisko: {nazwisko}, wyniki: {wyniki}");
+            }
+            return builder.ToString();
+        }
+
+
+    }
+}
