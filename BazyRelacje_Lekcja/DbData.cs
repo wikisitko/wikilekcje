abe3d581a5e3c378e42ded1cb6363dd1f1861516
Radek <weresa@gmail.com>
2021-03-29 22:19:21 +0200

    poprawka do db

--- BazyRelacje_Lekcja/DbData.cs
+++ BazyRelacje_Lekcja/DbData.cs
@@ -7,40 +7,35 @@ namespace BazyRelacje_Lekcja
 {
     class DbData
     {
+        private DB context;
+
+        public DbData()
+        {
+            context = new DB();
+        }
+
         public List<Client> GetAllClients()
         {
-            using(DB context = new DB())
-            {
                 var clients = context.Clients.ToList();
                 return clients;
-            }
         }
 
         public List<Complaint> GetAllComplaints()
         {
-            using (DB context = new DB())
-            {
                 var complaints = context.Complaints.ToList();
                 return complaints;
-            }
         }
 
         public void AddClient(Client client)
         {
-            using (DB context = new DB())
-            {
                 context.Clients.Add(client);
                 context.SaveChanges();
-            }
         }
 
         public void AddComplaint(Complaint complaint)
         {
-            using (DB context = new DB())
-            {
                 context.Complaints.Add(complaint);
                 context.SaveChanges();
-            }
         }
     }
 }
