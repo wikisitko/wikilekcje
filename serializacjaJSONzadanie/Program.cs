d6503d3eb63ff312b6a523eb3ba462cad88dd07a
Wiktoria Sitko <wsitko2000@gmail.com>
2021-06-28 22:58:10 +0200

    serializacja danych JSON

---
+++ serializacjaJSONzadanie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace serializacjaJSONzadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
