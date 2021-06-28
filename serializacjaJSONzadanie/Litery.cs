39889a16bbac99f3632be666a428f0fce5fb8e04
Wiktoria Sitko <wsitko2000@gmail.com>
2021-06-28 23:22:35 +0200

    dodanie klasy - serializacja danych json

---
+++ serializacjaJSONzadanie/Litery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace serializacjaJSONzadanie
+{
+    public class Litery
+    {
+        [JsonProperty("eighty_min_score")]
+        public string EightyMinScore { get; set; }
+        [JsonProperty("home_or_away")]
+        public string HomeOrAway { get; set; }
+        [JsonProperty("score ")]
+        public string Score { get; set; }
+        [JsonProperty("team_id")]
+        public string TeamId { get; set; }
+    }
+}
---
+++ serializacjaJSONzadanie/Przypadek.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace serializacjaJSONzadanie
+{
+    public class Przypadek
+    {
+        private int id;
+        private DateTime date;
+        private string voivodeship;
+        private string region;
+        private string gender;
+        private string description;
+
+        public int ID { get => id; set => id = value; }
+        public DateTime DATE { get => date; set => date = value; }
+        public string VOIVODESHIP { get => voivodeship; set => voivodeship = value; }
+        public string REGION { get => region; set => region = value; }
+        public string GENDER { get => gender; set => gender = value; }
+        public string DESCRIPTION { get => description; set => description = value; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
